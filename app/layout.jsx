28825815b934806d9ef14845161218d69175a177
Craz301 <128047647+Craz301@users.noreply.github.com>
2025-02-17 02:37:21 +0800

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/global.css';
+import Nav from '@components/Nav.jsx';
+import Provider from '@components/Provider.jsx';
+import Sidebar from '@components/Sidebar';
+
+
+export const metadata = {
+    title:"Mindscribe",
+    description:"An intuitive note application for managing and storing your notes seamlessly",
+    logo: "/assets/images/logo.svg"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div>
+           {children}
+          </div>
+       
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
---
+++ app/notes/create-note/page.jsx
@@ -0,0 +1,62 @@
+"use client"
+import {useState} from 'react'
+import { useSession} from "next-auth/react"
+import { useRouter } from 'next/navigation'
+import Form from "@components/Form"
+
+const CreateNote = () => {
+    const router = useRouter();
+    const {data:session , status} = useSession();
+    const [submitting, setSubmitting]  = useState(false) ;
+    const [note, setNote] = useState({title: '',body:'', bgColor:''})
+
+    if (status === "loading") {     
+        return <p>Loading...</p>
+    }
+
+    if (status === "unauthenticated") {
+        return <p>Access Denied</p>
+    }
+
+    const storeNote = async (e) => {
+        e.preventDefault(); // prevent default form submit reload
+        setSubmitting(true);
+        router.refresh();
+        try {
+            const response = await fetch("/api/note/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    userId: session?.user.id,
+                    noteTitle:note.title,
+                    noteBody:note.body,
+                    bgColor:note.bgColor,
+                    isShown: 1, // Set isShown to 1 to show the note
+                })
+            })
+            if(response.ok){
+                router.push('/');
+            } else {
+                console.log(response)
+            }
+        } catch(error){
+            console.log(response)
+            console.log(error)
+        } finally {
+            setSubmitting(false);
+        }
+
+    }
+        return (
+         
+            <Form 
+            type="Create"
+            note={note}
+            setNote={setNote}
+            submitting={submitting}
+            handleSubmit={storeNote}
+        />
+    
+        )
+}
+
+export default CreateNote
